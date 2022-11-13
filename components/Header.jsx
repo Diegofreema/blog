b9f0d65f5a2bb57a4f2c45585a1df0cfa4227396
diegofreema <diegofreeman78@gmail.com>
2022-11-13 06:39:09 +0100

    another commit

---
+++ components/Header.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import  { useState, useEffect } from 'react';
+
+import Link from 'next/link';
+import { getCategories } from '../services';
+
+
+
+const Header = () => {
+
+const [categories, setCategories] = useState([]);
+useEffect(() => {
+  getCategories().then((newCategories) => setCategories(newCategories));
+}, []);
+
+
+  return (
+    <div className="container mx-auto px-10 mb-8">
+      <div className="border-b w-full flex flex-row justify-between border-blue-400 py-8">
+        <div className="">
+          <Link href="/">
+            <span className="cursor-pointer font-bold text-4xl text-white ">
+              MyBlog
+            </span>
+          </Link>
+        </div>
+        <div className="hidden  sm:flex">
+          {categories.map((category) => (
+            <Link key={category.slug} href={`/category/${category.slug}`}>
+              <span className="md:float-right mt-2 align-middle text-white ml-4 font-semibold cursor-pointer">
+                {category.name}
+              </span>
+            </Link>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
